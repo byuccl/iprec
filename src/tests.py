df587ff2fb684b2d50736c06a2afc9325db76610
ReillyMcK <romckend@byu.edu>
2022-11-21 19:17:58 -0700

    Initial setup

---
+++ src/tests.py
@@ -0,0 +1,28 @@
+"""
+Unit testing for IPRec functions
+"""
+
+import unittest
+
+
+class TestCompareV(unittest.TestCase):
+    def test_import_design(self):
+        pass
+
+    def test_compare_eqn(self):
+        pass
+
+    def test_compare_ref(self):
+        pass
+
+    def test_is_constant_vertex(self):
+        pass
+
+    def test_get_edge_dict(self):
+        pass
+
+    def test_compare_vertex(self):
+        pass
+
+    def test_print_graph(self):
+        pass
