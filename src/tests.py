3cff6a12696c6bea0116a87ebdca24d26dda5780
ReillyMcK <romckend@byu.edu>
2022-11-28 23:04:30 -0700

    Test compare_v refactor

--- src/tests.py
+++ src/tests.py
@@ -13,6 +13,7 @@ import unittest
 
 from config import ROOT_PATH, TEST_RESOURCES, RECORD_CORE_TCL
 from compare_v import import_design
+from compare_v_refactor import import_design as import_design_refactor
 
 
 class TestCompareV(unittest.TestCase):
@@ -23,22 +24,41 @@ class TestCompareV(unittest.TestCase):
     vertex_attr = {"label", "name", "IS_PRIMITIVE", "color", "ref", "parent"}
     edge_attr = {"parent", "in_pin", "out_pin", "signal"}
 
-    def test_import_design(self, test_function=import_design):
-        """
-        Test import design against design imported from previous commit.
-        """
-        actual_graph = None
-        with open(TEST_RESOURCES / "aes128" / "iprec_output" / "aes128.pkl", "rb") as f:
-            actual_graph = Graph.Read_Pickle(f)
-        self.assertTrue(actual_graph)
-
+    def test_import_design_refactor(self):
+        self.test_import_design(test_function=import_design_refactor, flat=True)  # type:ignore
         data = {}
         with open(TEST_RESOURCES / "aes128" / "iprec_output" / "aes128.json", "r") as f:
             data = json.load(f)
+        actual_graph = None
+        with open(TEST_RESOURCES / "aes128" / "iprec_output" / "aes128.pkl", "rb") as f:
+            actual_graph = Graph.Read_Pickle(f)
+        self.test_import_design(
+            test_function=import_design_refactor,  # type:ignore
+            data=data,
+            actual_graph=actual_graph,
+            flat=False,
+        )
+
+    def test_import_design(self, test_function=import_design, data={}, actual_graph=None, **kwargs):
+        """
+        Test import design against design imported from previous commit.
+        """
+        if actual_graph is None:
+            with open(TEST_RESOURCES / "aes128" / "iprec_output" / "aes128_flat.pkl", "rb") as f:
+                actual_graph = Graph.Read_Pickle(f)
+        self.assertTrue(actual_graph)
+
+        if not data:
+            with open(TEST_RESOURCES / "aes128" / "iprec_output" / "aes128_flat.json", "r") as f:
+                data = json.load(f)
         self.assertTrue(data)
-        test_graph = test_function(data)
+        test_graph = test_function(data, **kwargs)
 
         self.assertEqual(test_graph.is_directed(), actual_graph.is_directed())
+
+        with open(TEST_RESOURCES / "test_graph.pkl", "wb") as f:
+            test_graph.write_pickle(f)
+
         for test_vertex in test_graph.vs():
             actual_vertex = actual_graph.vs(name=test_vertex["name"])
             self.assertEqual(
@@ -79,10 +99,14 @@ class TestCompareV(unittest.TestCase):
                 for actual_edge in actual_edges:
                     if (
                         actual_edge.source_vertex["name"] == (test_edge.source_vertex["name"])
-                        and actual_edge.target_vetex["name"] == test_edge.target_vertex["name"]
+                        and actual_edge.target_vertex["name"] == test_edge.target_vertex["name"]
                     ):
-                        test_attr = {k: v for k, v in test_edge.attributes() if k != "ports"}
-                        actual_attr = {k: v for k, v in actual_edge.attributes() if k != "ports"}
+                        test_attr = {
+                            k: v for k, v in test_edge.attributes().items() if k != "ports"
+                        }
+                        actual_attr = {
+                            k: v for k, v in actual_edge.attributes().items() if k != "ports"
+                        }
                         results = dict(test_attr.items() & actual_attr.items())
                         if not self.edge_attr < results.keys():
                             continue
@@ -111,3 +135,7 @@ class TestCompareV(unittest.TestCase):
 
     def test_print_graph(self):
         pass
+
+
+if __name__ == "__main__":
+    unittest.main()
